100ae81a9461e21ef97ccfaa2f0a9fb166c037ba
saeed <khastavan@outlook.com>
2020-09-17 23:20:27 +0430

    complementary.dart in other sources modified

--- other sources/complementary.dart
+++ other sources/complementary.dart
@@ -1,14 +1,4 @@
-/* 
-void main() {
-  String name1; // name1 is non-nullable
-  print(name1); // error
 
-  String?
-      name2; // name2 is nullable. this line equals with: String? name2 = null;
-  print(name2); // prints null
-  print(name2.length);  // error
-}
- */
 /* 
 void main() {
   int? mojoodi;
---
+++ other sources/null_variable.dart
@@ -0,0 +1,13 @@
+// null variable
+
+/* 
+void main() {
+  String name1; // name1 is non-nullable
+  print(name1); // error
+
+  // name2 is nullable. this line equals with: String? name2 = null;
+  String? name2; 
+  print(name2); // prints null
+  print(name2.length);  // error
+}
+ */
