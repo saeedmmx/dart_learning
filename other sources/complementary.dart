71f3fc32a7e61e748a3035fbfe432029e20f7cc3
saeed <khastavan@outlook.com>
2020-09-17 23:31:42 +0430

    some codes organized in null_variable.dart

--- other sources/complementary.dart
+++ other sources/complementary.dart
@@ -1,27 +1,6 @@
 
-/* 
-void main() {
-  int? mojoodi;
-  print(mojoodi + 100); //compile time error because mojoodi can be null.
-  if (mojoodi != null) {
-    print(mojoodi + 100);
-  } // It will run without problem.
-}
- */
-/* 
-void main() {
-  String? userName;
-  print(userName);
-  print(userName.length); // compile time error
-   
-  // With conditional access operator you say, if userName isn't null then calculate its length.
-  print(userName?.length);  
 
-  // Null check operator used on a null value. 
-  // With assertion operator(!), you say I'm sure it's not null.
-  print(userName!.length);  
-}
- */
+
 /* 
 // Use a function in another function.
 void main() {
--- other sources/null_variable.dart
+++ other sources/null_variable.dart
@@ -1,4 +1,4 @@
-// null variable
+// dealing with null values.
 
 /* 
 void main() {
@@ -9,5 +9,31 @@ void main() {
   String? name2; 
   print(name2); // prints null
   print(name2.length);  // error
+}
+ */
+
+/* 
+void main() {
+  int? capacity;
+  print(capacity + 100); //compile time error because capacity can be null.
+  
+  if (capacity != null) {
+    print(mojoodi + 100);
+  } // It will run without problem.
+}
+ */
+
+/* 
+void main() {
+  String? userName;
+  print(userName); // prints null
+  print(userName.length); // compile time error
+   
+  // With conditional access operator you say, if userName isn't null then calculate its length.
+  print(userName?.length);  // prints null
+
+  // Null check operator used on a null value. 
+  // With assertion operator(!), you say I'm sure it's not null.
+  print(userName!.length);  // error
 }
  */
