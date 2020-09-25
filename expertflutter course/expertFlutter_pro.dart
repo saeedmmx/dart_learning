cd0c74f3c0701b17a8e81e85a2d3ba072a348e64
saeed <khastavan@outlook.com>
2020-09-25 11:25:37 +0330

    library and loops files created in expertflutter

--- expertflutter course/expertFlutter_pro.dart
+++ expertflutter course/expertFlutter_pro.dart
@@ -1,31 +1,7 @@
 
 
 
-/* 
-void main() {
-  var numberList = [8, 10, 2, 11, 5, 9];
-  var biggestNumber = numberList[0];
 
-  for (int i = 0; i < numberList.length; i++) {
-    if (numberList[i] > biggestNumber) {
-      biggestNumber = numberList[i];
-    }
-  }
-  print('The biggest number is: $biggestNumber');
-  print('Index of biggest number is: ${numberList.indexOf(biggestNumber)}');
-}
- */
-/* 
-import 'dart:math';
-
-void main() {
-  int a = 5;
-  int b = 3;
-
-  num result = pow(a, b);
-  print(result);
-}
- */
 /* 
 void main() {
   if (canYouDrive(23)) {
---
+++ expertflutter course/library.dart
@@ -0,0 +1,13 @@
+// example 1: math library
+/* 
+import 'dart:math';
+
+void main() {
+  int a = 5;
+  int b = 3;
+
+  num result = pow(a, b);
+  print(result);
+}
+ */
+
