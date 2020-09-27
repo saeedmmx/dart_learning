b85caf75f1fdebfd643baed71882ffbc52f7a2f4
saeed <khastavan@outlook.com>
2020-09-27 21:19:34 +0330

    const-final.dart created & some codes moved to it

---
+++ expertflutter course/const-final.dart
@@ -0,0 +1,23 @@
+// example 1
+/* void main() {
+  final myName = 'Saman khoobi';
+
+  print(myName.length);
+}
+ */
+
+//example 2
+/* void main() {
+  final name = 'Amir';
+  final nameLength = name.length;
+  print(nameLength);
+
+  const productName = 'pillow';
+  const productNameLength = productName.length; // It is possible because productName is a constant variable.
+  print(productNameLength);
+
+  final lastName = 'miri';
+  const lastNameLength = lastName.length; // error: const variable must be initialized with a constant value.
+  print(productNameLength);
+}
+ */
