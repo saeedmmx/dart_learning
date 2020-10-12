31eafde695b1d4804d8d0ddcbe388758eac23857
saeed <khastavan@outlook.com>
2020-10-12 23:38:46 +0330

    oop-general created and an example added

--- expertflutter course/expertFlutter_pro.dart
+++ expertflutter course/expertFlutter_pro.dart
@@ -4,30 +4,7 @@
 
 // OOP
 
-/* 
-void main() {
-  var car = new Car();  // first instance
-  car.start();
 
-  Car car2 = Car();  // second instance
-  car2.color = 'red';
-  print(car2.color);
-}
-
-class Car {
-  int? topSpeed;
-  String? modelNumber;
-  String? color;
-
-  void start() {
-    print('starting ...');
-  }
-
-  void stop() {
-    print('stoping ...');
-  }
-}
- */
 /* 
 // 'this' keyword
 
---
+++ expertflutter course/oop-general.dart
@@ -0,0 +1,27 @@
+// example 1
+/* 
+void main() {
+  var car = new Car();  // first instance
+  car.start();
+
+  Car car2 = Car();  // second instance
+  car2.color = 'red';
+  print(car2.color);
+}
+
+class Car {
+  int? topSpeed;
+  String? modelNumber;
+  String? color;
+
+  void start() {
+    print('starting ...');
+  }
+
+  void stop() {
+    print('stoping ...');
+  }
+}
+ */
+
+// example 2
